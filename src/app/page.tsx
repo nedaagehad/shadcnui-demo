96451bb8fec1349f715ce46be2ae38017efc3339
Nedaa Gehad <nedaagehd@gmail.co>
2025-01-28 15:36:30 +0200

    create dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,59 +1,13 @@
 "use client"
 
-import { Button } from "@/components/ui/button";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useRouter } from "next/navigation";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-
-const formSchema = z.object({
-  username: z.string().min(3, "Must be at least 3 characters!").max(50, "Must be 50 characters maximum!"),
-  password: z.string().min(8, "must be at least 8 characters!").max(20, "Must be max of 20 characters")
-})
+import Dashboard from "./dashboard/page";
+import Login from "./login/page"
 
 export default function Home() {
-  const router = useRouter()
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      username: "",
-      password: ""
-    },
-  })
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    
-    console.log(values)
-    router.push("/campaigns")
-  }
+  const userAuthenticated = localStorage.getItem("loggedIn")
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] justify-items-center min-h-screen p-30 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField control={form.control} name="username" render={({field}) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="Enter your username..." {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )} />
-          <FormField control={form.control} name="password" render={({field}) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <Input placeholder="Enter your password..." {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )} />
-          <Button type="submit">Submit</Button>
-        </form>
-      </Form>
-    </div>
+    !userAuthenticated ? <Login /> : <Dashboard />
   );
 }
